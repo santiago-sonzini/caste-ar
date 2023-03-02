dc8da48fbe9912164c8f0f11ada5d729b98ec593
david s <santisonzini1234@gmail.com>
2023-03-02 16:18:29 -0300

    first-commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.set('/', 'html')
+app.use(express.static(path.join(__dirname, '/')))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req,res)=>{
+    console.log('g');
+    res.render('index')
+})
+
+app.listen(8080, ()=>{
+    console.log('listening on http://localhost:8080/');
+})
